e9d43470c47a5664648358846ed67980d714383b
nikunj bhuva <nikunjbhuva121@gmail.com>
2024-05-29 23:01:28 +0530

    basic calculation project

---
+++ index.js
@@ -0,0 +1,44 @@
+// que 1 //
+
+// let userInput = +prompt("enter your first Number: ");
+// let symbolInput = prompt("enter your symbol: ");
+// let userInput2 = +prompt("enter your second Number: ");
+
+// if (symbolInput === "+") {
+//     console.log("Addition is: ", userInput + userInput2);
+// }
+// else if (symbolInput === "-") {
+//     console.log("Substraction is: ", userInput - userInput2);
+// }
+// else if (symbolInput === "*") {
+//     console.log("Multiplication is: ", userInput * userInput2);
+// }
+// else if (symbolInput === "/") {
+//     console.log("Divison is: ", userInput / userInput2);
+// }
+// else {
+//     console.log("Invalid symbol");
+// }
+
+// que 2 //
+
+// let number1 = 10;
+// let number2 = 20;
+
+// number1 = number1 + number2;//10+20=30
+// number2 = number1 - number2;//30-20=10
+// number1 = number1 - number2;//30-10=20
+// console.log("number1 is swap successfully: ",number1);
+// console.log("number2 is swap successfully: ", number2);
+
+// que 3 //
+
+// let number=+prompt("enter your Number: ");
+// console.log("Your square is:",number*number);
+
+// que 4 //
+
+// let fAngle = 45;
+// let sAngle = 45;
+// let tAngle = 180 - (fAngle + sAngle);
+// console.log("Your third angle is: ",tAngle);
